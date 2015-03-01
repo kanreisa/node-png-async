fa2bedaf8385ccb42ca786f310665cafd3f62503
Yuki KAN <re@pixely.jp>
2015-03-01 22:13:18 +0900

    gulpfile.js, tsd.json

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+/*jslint node:true, white:true */
+'use strict';
+
+var fs = require('fs');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var mocha = require('gulp-mocha');
+var typescript = require('gulp-typescript');
+var merge = require('merge2');
+
+gulp.task('tsc', function () {
+
+    var result = gulp.src('src/*.ts').pipe(typescript({
+        target: 'ES6',
+        module: 'commonjs',
+        declarationFiles: true
+    }));
+
+    return merge([
+        result.dts.pipe(gulp.dest('lib')),
+        result.js.pipe(gulp.dest('lib'))
+    ]);
+});
+
+function test() {
+    return gulp.src(['test/*.js']).pipe(mocha({ reporter: 'spec' }));
+};
+gulp.task('test', test);
+
+gulp.task('build', ['tsc']);
+
+gulp.task('watch', function () {
+    gulp.watch('src/*.ts', ['default']);
+    gulp.watch('test/*.js', ['test']);
+});
+
+gulp.task('default', ['build'], test);
